35c8a5051d3304e2c1a7e5afd664ac1d38db641e
ashutosh <33460654+ashu-pattanayak@users.noreply.github.com>
2022-07-20 20:07:04 +0530

    Task 3

--- automation.sh
+++ automation.sh
@@ -1,72 +1,78 @@
-s3_bucket="upgrad-ashutosh"
-myname="ashutosh"
-echo "Updating the packages"
-run_sudo=$(sudo apt update -y)
-echo "$run_sudo"
+apt update -y
 
-echo "installing apache2"
-install_apache2=$(sudo apt install apache2 -y)
-echo "$install_apache2"
+myname='ashutosh'
+s3_bucket='upgrad-ashutosh'
+timestamp=$(date '+%d%m%Y-%H%M%S')
 
-echo "Starting apache2...."
-sudo systemctl start apache2
-
-
-echo "Check if Apache2 is running or not"
-if sudo systemctl status apache2 | grep "active (running)"; then
-    echo "Apache2 service running"
+if [ $(dpkg --list | grep apache2 | cut -d ' ' -f 3 | head -1) == 'apache2' ]
+then
+        echo "Apache2 is installed."
+        if [[ $(systemctl status apache2 | grep disabled | cut -d ';' -f 2) == ' disabled' ]];
+                then
+                        systemctl enable apache2
+                        echo "Apache2 enabled"
+                        systemctl start apache2
+                else
+                        if [ $(systemctl status apache2 | grep active | cut -d ':' -f 2 | cut -d ' ' -f 2) == 'active' ]
+                        then
+                                echo "Apache2 is already running"
+                        else
+                                systemctl start apache2
+                                echo "Apache2 started"
+                        fi
+        fi
 else
-    echo "Apache2 service not running"
+        printf 'Y\n' | apt-get install apache2
+        echo "Apache2 service is installed"
 fi
 
-timestamp="$(date '+%d%m%Y-%H%M%S')"
-filename="/tmp/${myname}-httpd-logs-${timestamp}.tar"
 
 
-echo "Creating Tar file "
-
-tar -cf ${filename} $( find /var/log/apache2/ -name "*.log")
+tar -zvcf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/*.log
 
 
-filesize=$(wc -c $filename | awk '{print $1}')
+if [ $(dpkg --list | grep awscli | cut -d ' ' -f 3 | head -1) == 'awscli' ]
+        then
+                aws s3 \
+                cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
+                s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
 
-echo "uploading to S3"
-aws s3 cp ${filename} s3://${s3_bucket}/${filename}
+        else
+        printf 'Y\n' | apt install awscli
+        aws s3 \
+        cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
+        s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
 
-echo "uploading to S3 is done"
-
-inventory_file="/var/www/html/inventory.html"
-
-echo "checking the inventory file"
-
-if [[ ! -f $inventory_file ]]; then
-    echo "Inventory file is not found hence creating one" 
-    sudo touch $inventory_file
-    sudo chmod 777 $inventory_file
-    sudo echo "Log Type&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Time Created&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Type&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Size<br>" >> $inventory_file
 fi
 
-sudo echo "httpd-logs&nbsp;&nbsp;&nbsp;&nbsp;$timestamp&nbsp;&nbsp;&nbsp;&nbsp;tar&nbsp;&nbsp;&nbsp;&nbsp;$filesize Bytes<br>" >> $inventory_file
 
-echo "inventory file is updated"
+if [ -f "/var/www/html/inventory.html" ]
+        then
+                printf "<p>" >> /var/www/html/inventory.html
+                printf "\n\t$(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 2,3 | tail -1)" >> /var/www/html/inventory.html
+                printf "\t\t$(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 4,5 | cut -d '.' -f 1 | tail -1)" >> /var/www/html/inventory.html
+                printf "\t\t\t $(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 4,5 | cut -d '.' -f 2 | tail -1 )" >> /var/www/html/inventory.html
+                printf "\t\t\t\t$(ls -lrth /tmp/ | grep httpd | cut -d ' ' -f 6 | tail -1)" >> /var/www/html/inventory.html
+                printf "</p>" >> /var/www/html/inventory.html
+        else
+                touch /var/www/html/inventory.html
+                printf "<p>" >> /var/www/html/inventory.html
+                printf "\tLog-Type\tDate-Created\tType\tSize" >> /var/www/html/inventory.html
+                printf "</p>" >> /var/www/html/inventory.html
+                printf "<p>" >> /var/www/html/inventory.html
+                printf "\n\t$(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 2,3 | tail -1)" >> /var/www/html/inventory.html
+                printf "\t\t$(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 4,5 | cut -d '.' -f 1 | tail -1)" >> /var/www/html/inventory.html
+                printf "\t\t\t $(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 4,5 | cut -d '.' -f 2 | tail -1)" >> /var/www/html/inventory.html
+                printf "\t\t\t\t$(ls -lrth /tmp/ | grep httpd | cut -d ' ' -f 6 |tail -1)" >> /var/www/html/inventory.html
+                printf "</p>" >> /var/www/html/inventory.html
+fi
 
-cron_file="/etc/cron.d/automation"
-automation_file="/root/Automation_Project/automation.sh"
 
-echo "checking cron job"
 
-cron_job_exists=$(sudo crontab -l | grep 'automation')
-
-echo "cron job found : $cron_job_exists"
-
-if [[ ! $cron_job_exists ]]; then
-	if [[ ! -f  $cron_file ]]; then
-		echo "cron_file file is not found hence creating one"
-		sudo touch $cron_file
-		sudo chmod 777 $cron_file
-		sudo echo "00 11 * * * $automation_file" >> $cron_file
-	fi
-
-	echo "registering cronjob"
-	sudo crontab $cron_file
+if [ -f "/etc/cron.d/automation" ];
+        then
+                echo "Automation at 00:00 hrs"
+        else
+                touch /etc/cron.d/automation
+                printf "0 0 * * * root /root/Automation_Project/auotmation.sh" > /etc/cron.d/automation
 fi
